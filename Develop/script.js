7f503ffa445c9aec870b2789d2f1196628d45f56
LeonathaTurtle <mancillh@gmail.com>
2024-06-01 23:38:37 -0500

    figured out a way to return array from user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,68 +1,40 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
-/*addEmployeesBtn.onclick = function() {
-  const employeesArray = collectEmployees();
-}*/
+addEmployeesBtn.addEventListener('click', collectEmployees);
 
-// Collect employee data
 function collectEmployees() {
-  const employeesArray=[]
-  
-  while (true) {
-    const firstNameInput = prompt ("Enter first name");
-    if (firstNameInput === null) break;
-    const lastNameInput = prompt ("Enter last name");
-    if (lastNameInput === null) break;
-    let salaryInput = prompt ("Enter salary");
-    if (salaryInput === null) break;
+
+  while (confirm("Do you want to add another employee?")) {
+
+    const firstNameInput = prompt("Enter first name");
+    const lastNameInput = prompt("Enter last name");
+    let salaryInput = prompt("Enter salary");
 
     if (isNaN(salaryInput)) {
       salaryInput = 0;
-    } else {
-    salaryInput = parseFloat(salaryInput);
-    }
+  } else {
+      salaryInput = Number(salaryInput);
+  }
 
-    const employee = {
+    const employeesArray = {
       firstName: firstNameInput,
       lastName: lastNameInput,
       salary: salaryInput
     };
-  employeesArray.push(employee);
 
-  // Asks user if they want to add another employee
-  addEmployee = confirm("Do you want to add another employee?");
-
-}
-
-return employeesArray;
+  console.log (employeesArray);
+  return employeesArray;
   
-}
-  // TODO: Get user input to create and return an array of employee objects
-  
-  
-  /*while(let i = 0; i < 3; i++) {
-    const response = prompt(employeesArray[i]);
-    userResponses[employeesArray[i]] = response;
-  }
 
-  
-};
-
-// Display the average salary
 const displayAverageSalary = function(employeesArray) {
-  // TODO: Calculate and display the average salary
- sum (employeesArray[2])/employeesArray.length
+ 
 }
-
+  }
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
-  const index = Math.floor(Math.random() * .length);
-  const computerChoice = [0] + [1];
 }
-
-window.alert("The randomly selected employee is " + computerChoice)
-*/
+}
 /*
   ====================
   STARTER CODE
