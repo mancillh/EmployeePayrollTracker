421b89554d4c059ea710fa247ab2692ac44953d3
LeonathaTurtle <mancillh@gmail.com>
2024-06-03 23:49:15 -0500

    adjusted file structure, reconnected js and css to index, commented throughout js

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This website allows the user to view their employees' payroll data and properly 
 
 When the user clicks the "Add employee" button, they are prompted to enter first name, last name, and salary. When the user finishes adding an employee, they are prompted to continue or cancel. When they are finished entering new employee information, their employee data is displayed on the page sorted alphabetically by last name, and the console shows the average employee salary and chooses an employee at random. 
 
-![Screenshot Employee Payroll Tracker](image.png)
+![Screenshot Employee Payroll Tracker](screenshot_employee_payroll.JPG)
--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,23 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
+//This function prompts the user for information and returns an array
 function collectEmployees() {
 
+  // begin with an empty array
   const employeesArray = [];
 
+  // simple boolean to begin access to the while loop when the site first loads
   let start = true
 
+  // while the user has indicated they want to add an employee...
   while (start) {
 
     const firstNameInput = prompt("Enter first name");
     const lastNameInput = prompt("Enter last name");
     const salaryInput = prompt("Enter salary");
 
+    // if the salary is not input as a number, enter 0
     if (isNaN(salaryInput)) {
       salaryInput = 0;
     } 
@@ -20,32 +25,42 @@ function collectEmployees() {
     const employeeInfo = {
       firstName: firstNameInput,
       lastName: lastNameInput,
-      salary: parseInt(salaryInput)
+      salary: parseInt(salaryInput) //parseInt converts a string number into an integer
     };
-
+    
+    // pushes the values from the employeeInfo object into an array called employeesArray
     employeesArray.push(employeeInfo);
 
+    // prompts the user to add another employee. If yes, while loop restarts. If not, exit loop
     start = confirm("Do you want to add another employee?")
   }
+  // returns an employeeArray, each index is an employee with three properties (first name, last name, salary)
   return employeesArray;
 }
 
+//Displays Average Salary
 const displayAverageSalary = function (employeesArray) {
 
   let total = 0;
 
+  // this for loop provides the sum of all employees' salaries
   for (i = 0; i < employeesArray.length; i++) {
     total += employeesArray[i].salary
   }
 
+  // to get the average, divide the sum of the salaries from above by the number of employees entered
   const average = total/employeesArray.length
+
+  // this logs the average salary in the console
   console.log("The average salary of our employees is $" + average + ".00");
 }
 
-const getRandomEmployee = function (employeesArray) {
-  let computerChoiceNum = employeesArray[Math.floor(Math.random() * employeesArray.length)];
- 
-  console.log(`Congratulations to ${computerChoiceNum.firstName} ${computerChoiceNum.lastName} our random drawing winner!`);
+// Chooses Random Employee
+  const getRandomEmployee = function (employeesArray) {
+  // computer chooses a number between 0 and the length of the array which is how many employees have been added
+  let computerChoice = employeesArray[Math.floor(Math.random() * employeesArray.length)];
+  // this logs the randomly selected employee into the console and also associates the number randomly chosen by the computer with an employee's name
+  console.log(`Congratulations to ${computerChoice.firstName} ${computerChoice.lastName} our random drawing winner!`);
 }
 
 
